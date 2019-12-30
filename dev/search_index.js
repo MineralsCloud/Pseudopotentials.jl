f2b63f5c62136fe83839272eadaa61963ea36911
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-12-30 04:19:04 +0000

    build based on 6c41543

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#","page":"Home","title":"Home","text":"CurrentModule = Pseudopotentials","category":"page"},{"location":"index.html#Pseudopotentials-1","page":"Home","title":"Pseudopotentials","text":"","category":"section"},{"location":"index.html#","page":"Home","title":"Home","text":"","category":"page"},{"location":"index.html#","page":"Home","title":"Home","text":"Modules = [Pseudopotentials]","category":"page"}]
+}
