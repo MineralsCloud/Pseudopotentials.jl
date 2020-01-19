24e2aaa64d22959ab630165e60ceb6f2da9764f3
Qi Zhang <singularitti@outlook.com>
2020-01-18 23:31:47 -0500

    Add `UnifiedPseudopotentialFormat`, its subtypes and `pseudopot_format`
    from QuantumESPRESSOBase.jl

--- src/Pseudopotentials.jl
+++ src/Pseudopotentials.jl
@@ -1,5 +1,59 @@
 module Pseudopotentials
 
+export UnifiedPseudopotentialFormat,
+    VanderbiltUltraSoft, AndreaDalCorso, OldNormConserving, pseudopot_format
+
+"""
+    PseudopotentialFormat
+Represent all possible pseudopotential file formats.
+"""
+abstract type PseudopotentialFormat end
+"""
+    UnifiedPseudopotentialFormat <: PseudopotentialFormat
+A singleton representing the new UPF format.
+If it doesn't work, the pseudopotential format is determined by
+the file name.
+"""
+struct UnifiedPseudopotentialFormat <: PseudopotentialFormat end
+"""
+    VanderbiltUltraSoft <: PseudopotentialFormat
+A singleton representing the Vanderbilt US pseudopotential code.
+"""
+struct VanderbiltUltraSoft <: PseudopotentialFormat end
+"""
+    AndreaDalCorso <: PseudopotentialFormat
+A singleton representing the Andrea Dal Corso's code (old format).
+"""
+struct AndreaDalCorso <: PseudopotentialFormat end
+"""
+    OldNormConserving <: PseudopotentialFormat
+A singleton representing the old PWscf norm-conserving format.
+"""
+struct OldNormConserving <: PseudopotentialFormat end
+
+"""
+    pseudopot_format(data::AtomicSpecies)::String
+Return the pseudopotential format.
+The pseudopotential file is assumed to be in the new UPF format.
+If it doesn't work, the pseudopotential format is determined by
+the file name:
+- "*.vdb or *.van": Vanderbilt US pseudopotential code
+- "*.RRKJ3": Andrea Dal Corso's code (old format)
+- none of the above: old PWscf norm-conserving format
+"""
+function pseudopot_format(data::AtomicSpecies)::PseudopotentialFormat
+    ext = uppercase(splitext(data.pseudopot)[2])
+    return if ext == ".UPF"
+        UnifiedPseudopotentialFormat()
+    elseif ext ∈ (".VDB", ".VAN")
+        VanderbiltUltraSoft()
+    elseif ext == ".RRKJ3"
+        AndreaDalCorso()
+    else
+        OldNormConserving()
+    end
+end
+
 # include("UPF.jl")
 include("PSlibrary.jl")
 
