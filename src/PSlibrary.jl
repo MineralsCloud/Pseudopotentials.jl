3f2075c124cf95fa526683607e664abc6a824a60
Qi Zhang <singularitti@outlook.com>
2020-01-19 06:32:50 -0500

    Fix the periodic table in `list_elements`

--- src/PSlibrary.jl
+++ src/PSlibrary.jl
@@ -192,8 +192,7 @@ function list_elements()
     K  Ca Sc Ti V  Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr
     Rb Sr Y  Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I  Xe
     Cs Ba    Hf Ta W  Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn
-    Fr Ra    Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og
-    Uue
+    Fr Ra
           La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu
           Ac Th Pa U  Np Pu
     """
